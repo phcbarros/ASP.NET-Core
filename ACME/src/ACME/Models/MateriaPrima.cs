af4ad50294862742b863237266063556344f65b2
= <=>
2016-11-14 18:45:27 -0200

    Criação do projeto e configuração do EF

---
+++ ACME/src/ACME/Models/MateriaPrima.cs
@@ -0,0 +1,10 @@
+﻿namespace ACME.Models
+{
+    public class MateriaPrima
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int EstoqueMinimo { get; set; }
+        public int EstoqueAtual { get; set; }
+    }
+}
---
+++ ACME/src/ACME/Repositories/AcmeContext.cs
@@ -0,0 +1,16 @@
+﻿using ACME.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACME.Repositories
+{
+    public class AcmeContext:DbContext
+    {
+        public DbSet<MateriaPrima> MateriasPrimas { get; set; }
+
+        public AcmeContext(DbContextOptions options)
+            :base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
